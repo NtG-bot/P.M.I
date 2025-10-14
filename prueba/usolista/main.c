2620018112e2f09e4e1d44fd56c8107a57b6e2fb
piliibuteler <butelerpilar@gmail.com>
2025-10-14 19:54:37 -0300

    Add files via upload

---
+++ prueba/usolista/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "listaenteros.h"
+
+void CargaInt(ListaEnt *b){
+    int i, n, p;
+    printf("ingrese cant a cargar:\n");
+    scanf("%d", &n);
+    while(!IsFullLista(b)){
+
+            printf("ingrese entero:\n");
+            scanf("%d",&p);
+            InsertLista( b, p );
+        }
+}
+
+ListaEnt MuestraInt(ListaEnt b){
+    int i;
+    ResetLista(*b);
+    for (i=0;i<b->aux;i++){
+        printf("entero:%d\n", CopyLista(b));
+    }
+}
+
+int main()
+{
+    ListaEnt b[MAXI];
+    CargaInt(&b);
+    MuestraInt(b);
+    return 0;
+}
