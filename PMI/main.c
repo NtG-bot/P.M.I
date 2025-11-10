629b48c35b0c89403a1204d86ca6836f1c9ee650
MatixdGT <149551397+MatixdGT@users.noreply.github.com>
2025-11-09 23:07:21 -0300

    Implement ID generation for movements
    Added function to generate a new movement ID.

--- PMI/main.c
+++ PMI/main.c
@@ -6,16 +6,22 @@
 #include "ListaVO_Movimiento.h"
 #include "Movimiento.h"
 
+long int generarNuevoIdMovimiento() {
+    static long int proximo_id = 1; // Se inicializa solo la primera vez EN ESTE ARCHIVO
+    return proximo_id++;
+}
+
+
 
 int main()
 {
     Lista_contacto Lcontactos;
     Lista_movimiento LMovimientos
-    int IDbus,opcion,
+    int IDbus,opcion,opcion2;
 
-    
     printf("Hello world!\n");
     return 0;
 }
+
 
 
