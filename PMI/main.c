7ede037149483ba0424f4f0905afa0c4be0daba5
MatixdGT <149551397+MatixdGT@users.noreply.github.com>
2025-11-09 22:33:56 -0300

    Update main.c

--- PMI/main.c
+++ PMI/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "Contacto.h"
+#include "listaEContacto.h"
+#include "ListaVO_Movimiento.h"
+#include "Movimiento.h"
+
 
 int main()
 {
+    Lista_contacto Lcontactos;
+    Lista_movimiento LMovimientos
+
+
+    
     printf("Hello world!\n");
     return 0;
 }
+
